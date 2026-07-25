f80ecca41dc9ed4d81a10c6bd801864481729d06
willboys <wiliyamsho001@gmail.com>
2026-07-25 21:42:02 +0700

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,1 +1,12 @@
+import asyncio
 
+async def main():
+    print("Halo TON BOT")
+    while True:
+        await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
